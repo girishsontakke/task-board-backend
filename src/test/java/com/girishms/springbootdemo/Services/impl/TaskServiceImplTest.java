d62093785f835928606deaac8f7deda10b73e092
girishsontakke <girishsontakke7@gmail.com>
2023-08-07 14:13:26 +0530

    used spy for objectMapper for injecting in taskServiceImpl

--- src/test/java/com/girishms/springbootdemo/Services/impl/TaskServiceImplTest.java
+++ src/test/java/com/girishms/springbootdemo/Services/impl/TaskServiceImplTest.java
@@ -4,45 +4,50 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.girishms.springbootdemo.Entities.Task;
 import com.girishms.springbootdemo.Repositories.TaskRepository;
-import com.girishms.springbootdemo.Services.TaskService;
 import com.girishms.springbootdemo.constants.TaskStatus;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.CALLS_REAL_METHODS;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class TaskServiceImplTest {
     @Mock
-    TaskRepository taskRepository;
-    ObjectMapper objectMapper;
-    private TaskService taskService;
-
+    private TaskRepository taskRepository;
+    @Spy
+    private ObjectMapper objectMapper;
     @InjectMocks
+    private TaskServiceImpl taskService;
     private Task task;
 
     @BeforeEach
     void setUp() {
-        objectMapper = new ObjectMapper();
-        taskService = new TaskServiceImpl(taskRepository, objectMapper);
         task = Task.builder()
                 .id(1L)
                 .title("First task")
                 .status(TaskStatus.TODO)
                 .description("First task for test")
                 .build();
+    }
+
+    @AfterEach
+    void tearDown() {
+        task = null;
     }
 
     @Test
@@ -67,19 +72,21 @@ class TaskServiceImplTest {
     }
 
     @Test
-    void testUpdateTask() {
-        when(taskRepository.findById(task.getId())).thenReturn(Optional.ofNullable(task));
-        when(taskRepository.save(any())).thenAnswer(invocationOnMock -> invocationOnMock.getArguments()[0]);
-
+    void testUpdateTask() throws JsonProcessingException {
+        mock(ObjectMapper.class, CALLS_REAL_METHODS);
         Task updatedTask = Task.builder()
                 .id(task.getId())
                 .title("Updated title")
                 .build();
+        when(taskRepository.findById(task.getId())).thenReturn(Optional.ofNullable(task));
+        when(taskRepository.save(any())).thenAnswer(invocationOnMock -> invocationOnMock.getArguments()[0]);
 
         Task result = taskService.updateTask(updatedTask);
 
         assertThat(result).isNotNull();
         assertThat(result.getTitle()).isEqualTo("Updated title");
+
+        // asserting that only provided fields in the updatedTask object is updated in the existing task;
         assertThat(result.getDescription()).isEqualTo("First task for test");
     }
 }
